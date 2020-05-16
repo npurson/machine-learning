b435d427fd8f0d6d5f7f32610007b48641f67f03
Natter Purson <natterpurson@qq.com>
2020-05-16 21:29:52 +0800

    Add binarization of Mnist and visualization

--- knn/main.py
+++ knn/main.py
@@ -1,5 +1,7 @@
 import os
 import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 from knn import Knn
 
 train_images = './train-images.idx3-ubyte'
@@ -9,17 +11,39 @@ test_labels = './t10k-labels.idx1-ubyte'
 
 def load_mnist():
     x_train = np.fromfile(open(train_images), np.uint8)
-    x_train = x_train[16:].reshape((60000, 28, 28, 1)).astype(np.int)
+    x_train = x_train[16:].reshape((60000, 28, 28)).astype(np.int)
     y_train = np.fromfile(open(train_labels), np.uint8)
     y_train = y_train[8:].reshape((60000)).astype(np.int)
     x_test = np.fromfile(open(test_images), np.uint8)[16:]
-    x_test = x_test.reshape((10000, 28, 28, 1)).astype(np.int)
+    x_test = x_test.reshape((10000, 28, 28)).astype(np.int)
     y_test = np.fromfile(open(test_labels), np.uint8)[8:]
     y_test = y_test.reshape((10000)).astype(np.int)
     return x_train[:10000], y_train[:10000], x_test[:200], y_test[:200]
 
 def main():
     x_train, y_train, x_test, y_test = load_mnist()
+
+    # binarization
+    # for i in tqdm(range(len(x_train))):
+    #     for j in range(28):
+    #         for k in range(28):
+    #             x_train[i][j][k] = 1 if x_train[i][j][k] > 177 else 0
+    # for i in tqdm(range(len(x_test))):
+    #     for j in range(28):
+    #         x_test[i][j].squeeze()
+    #         for k in range(28):
+    #             x_test[i][j][k] = 1 if x_test[i][j][k] > 177 else 0
+    
+    # plot
+    # ax = plt.subplots(nrows=10, ncols=10, sharex='all', sharey='all')[1].flatten()
+    # for i in range(10):
+    #     img = x_train[i]
+    #     ax[i].imshow(img, cmap='Greys', interpolation='nearest')
+    # ax[0].set_xticks([])
+    # ax[0].set_yticks([])
+    # plt.tight_layout()
+    # plt.show()
+
     knn = Knn()
     knn.fit(x_train, y_train)
     y_pred = knn.predict(x_test)
