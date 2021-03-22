adda377b5c8bc442e56397b4b0ba2111c426ad19
npurson <natterpurson@qq.com>
2021-03-22 04:02:54 -0400

    Update BPNN.

--- bpnn/bpnn.py
+++ bpnn/bpnn.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 
 
@@ -20,39 +19,36 @@ class FCLayer(object):
         return np.dot(x, self.w_[:, 1:].T) + self.w_[:, 0]      # (B, Din) x (Din, Dout) + (Dout) => (B, Dout);
 
     def backward(self, delta, eta, reg_lambda):
+        delta_ = np.dot(delta, self.w_[:, 1:])                  # (B, Dout) x (Dout, Din)
+        self.w_[:, 1:] *= 1 - eta * reg_lambda
         self.w_[:, 1:] -= eta * np.dot(delta.T, self.x_)        # (Dout, Din) - (Dout, B) x (B, Din)
-        self.w_[:, 0] -= eta * np.sum(delta, axis=0)            # (Dout, B) => (Dout, 1)
-        return np.dot(delta, self.w_[:, 1:])
+        self.w_[:, 0] -= eta * np.sum(delta, axis=0)            # (Dout) - ((B, Dout) => (Dout))
+        return delta_
 
 
 class BPNN(object):
 
     def __init__(self, lengths: list):
-        self.w1 = FCLayer(lengths[0], lengths[1])
-        self.w2 = FCLayer(lengths[1], lengths[2])
-        self.w3 = FCLayer(lengths[2], lengths[3])
+        self.layers = [FCLayer(lengths[i], lengths[i + 1]) for i in range(len(lengths) - 1)]
 
     def __call__(self, x):
         return self.forward(x)
 
     def forward(self, x):
-        self.x1_ = x
-        p = np.tanh(self.w1(x))
-        self.x2_ = p
-        p = np.tanh(self.w2(p))
-        self.x3_ = p
-        p = softmax(self.w3(p))
-        return p
+        self.xs = []
+        for i, layer in enumerate(self.layers):
+            self.xs.append(x)
+            x = layer(x)
+            x = np.tanh(x) if i != len(self.layers) - 1 else softmax(x)
+        return x
 
-    def backward(self, delta, eta, reg_lambda=None):
-        delta = self.w3.backward(delta, eta, reg_lambda)
-        delta *= (1 - self.x3_ ** 2)
-        delta = self.w2.backward(delta, eta, reg_lambda)
-        delta *= (1 - self.x2_ ** 2)
-        delta = self.w1.backward(delta, eta, reg_lambda)
+    def backward(self, delta, eta, reg_lambda=0):
+        for i, layer in enumerate(reversed(self.layers)):
+            delta = delta * (1 - self.xs[-i] ** 2) if i else delta
+            delta = layer.backward(delta, eta, reg_lambda)
 
     def predict(self, x):
-        return np.argmax(self.forward(x))
+        return np.argmax(self.forward(x), axis=1)
 
 
 if __name__ == '__main__':
